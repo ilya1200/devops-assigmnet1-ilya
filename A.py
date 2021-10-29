8fb37c88707cd67e7d6f9e6609f090cf6fb7cf5f
ilya1200 <ilya1200@gmail.com>
2021-10-29 19:39:27 +0300

    add exercise A

---
+++ A.py
@@ -0,0 +1,10 @@
+first = 7
+second = 44.3
+
+add = first + second
+mul = first * second
+div = second / first
+
+print(f"{first}+{second}=" + str(add))
+print(f"{first}*{second}=" + str(round(mul, 2)))
+print(f"{second}/{first}=" + str(round(div, 2)))
